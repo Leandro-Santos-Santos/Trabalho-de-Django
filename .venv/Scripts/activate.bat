e4603cefe78b9e6b992677b76478227e6399e8a3
Leandro-Santos-Santos <santosleandro1805@gmail.com>
2024-07-25 10:53:03 -0300

    views função das paginas da web

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\182300643\Desktop\Trabalho Django\Trabalho-de-Django\.venv
+set VIRTUAL_ENV=C:\Users\leand\OneDrive\Área de Trabalho\trabalho de Django\Trabalho-de-Django\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
--- myproject/principal/views.py
+++ myproject/principal/views.py
@@ -18,3 +18,60 @@ from django.contrib import messages
 def index(request):
      return render(request, 'index.html')
 # Create your views here.
+
+def template(request):
+    return render(request, 'templates/template.html')
+
+def sobre(request):
+    if not request.session.get('usuario_id'):
+            return redirect('login')
+    else:
+        return render(request, 'Sobre/sobre.html')
+
+
+
+
+def pedido(request):
+    if request.method == 'POST':
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            try:
+                # Estabelecer conexão com o banco de dados
+                oracle = conecta_no_banco_de_dados()
+
+                # Preparar consulta SQL e valores
+                nome = form.cleaned_data['nome']
+                email = form.cleaned_data['email']
+                mensagem = form.cleaned_data['mensagem']
+                sql = "INSERT INTO contatos (nome, email, mensagem) VALUES (%s, %s, %s)"
+                values = (nome, email, mensagem)
+
+                # Executar consulta SQL e confirmar alterações
+                cursor = oracle.cursor()
+                cursor.execute(sql, values)
+                oracle.commit()
+
+                # Mensagem de sucesso e redirecionamento
+                print(f"Dados do formulário salvos com sucesso!")
+                return HttpResponseRedirect('/')
+
+            except Exception as err:
+                # Manipular erros de banco de dados
+                print(f"Erro ao salvar dados no banco de dados: {err}")
+                mensagem_erro = "Ocorreu um erro ao processar o seu contato. Tente novamente mais tarde."
+                return render(request, 'erro.html', mensagem_erro=mensagem_erro), 500
+
+            finally:
+                # Fechar conexão com o banco de dados se estiver aberta
+                if oracle is not None:
+                    oracle.close()
+
+        else:
+            # Manipular dados de formulário inválidos
+            return render(request, 'pedido.html', {'form': form})
+
+    else:
+        # Renderizar formulário vazio
+        form = ContatoForm()
+        return render(request, 'pedido.html', {'form': form})
+    
